bec0672676751f5ed8de0b71ff0c66f1137981b7
MikerPa <844015228@qq.com>
2024-01-13 14:37:58 +0800

    commit test

---
+++ KunBlog/src/main/java/org/kun/kunblog/KunBlogApplication.java
@@ -0,0 +1,13 @@
+package org.kun.kunblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KunBlogApplication.class, args);
+    }
+
+}
---
+++ KunBlog/src/test/java/org/kun/kunblog/KunBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kun.kunblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
